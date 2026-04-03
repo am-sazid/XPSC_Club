bd372efe0bbf5c32c5577436efe28c36577679a7
am-sazid <amsazid.cse@gmail.com>
2026-04-04 00:30:23 +0600

    Solve 2 Problem

--- Week - 1/Approximate_Answer.cpp
+++ Week - 1/Approximate_Answer.cpp
@@ -1,8 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() 
+int main()
 {
-    
+    int x, y, k;
+    cin >> x >> y >> k;
+
+    int sub = x - y;
+    if (abs(sub) <= k)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
     return 0;
 }
---
+++ Week - 1/B_Minor_Change.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    string s,t;
+    cin>>s;
+    cin>>t;
+
+    int count = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if(s[i]!= t[i]){
+            count++;
+        }
+    }
+    
+    cout<<count<<endl;
+    return 0;
+}
