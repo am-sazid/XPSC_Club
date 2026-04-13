d1751f288cfea568ef2a87c4c3030ade5cd80bb4
am-sazid <amsazid.cse@gmail.com>
2026-04-13 14:47:03 +0600

    Week 1 Complete

--- Week - 1/B_Chemistry.cpp
+++ Week - 1/B_Chemistry.cpp
@@ -1,32 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr);
 
     int t;
     cin >> t;
-    while (t--) {
+
+    while (t--)
+    {
         int n, k;
         cin >> n >> k;
+
         string s;
         cin >> s;
 
         vector<int> freq(26, 0);
-        for (char c : s) freq[c - 'a']++;
 
-        int odd = 0;
-        for (int f : freq) {
-            if (f % 2) odd++;
+        for (char c : s)
+        {
+            freq[c - 'a']++;
         }
 
-        int need = max(0, odd - 1);
+        int odd = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            if (freq[i] % 2 != 0)
+            {
+                odd++;
+            }
+        }
 
-        if (k >= need && (k - need) % 2 == 0)
+        if (k >= odd - 1)
+        {
             cout << "YES\n";
+        }
         else
+        {
             cout << "NO\n";
+        }
     }
 
     return 0;
