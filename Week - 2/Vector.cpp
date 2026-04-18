6abd6f93dac46a370d6ab7376f6132a178f7ebd2
am-sazid <amsazid.cse@gmail.com>
2026-04-18 22:24:43 +0600

    Week 2

---
+++ Week - 2/Vector.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>> n;
+    vector<int> v;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<v[i]<<endl;
+    }    
+    
+    return 0;
+}
