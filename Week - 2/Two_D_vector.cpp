2203037a0d0473a5e9cee9fe32b2382578414897
am-sazid <amsazid.cse@gmail.com>
2026-04-25 17:46:08 +0600

    2D Vector

--- Week - 2/Two_D_vector.cpp
+++ Week - 2/Two_D_vector.cpp
@@ -6,21 +6,41 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m;
-    cin >> n >> m;
+    int n;
+    cin >> n;
 
-    vector<vector<int>> v(n, vector<int>(m, 1));
+    vector<vector<int>> v;
 
     for (int i = 0; i < n; i++)
     {
+        int m;
+
+        cin >> m;
+        vector<int> a;
         for (int j = 0; j < m; j++)
+        {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+
+        // for(auto value : a){
+        //     cout<< value<< " ";
+        // }
+        // cout<< endl;
+
+        v.push_back(a);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
         {
             cout << v[i][j] << " ";
         }
+
         cout << endl;
     }
-
-    
 
     return 0;
 }
