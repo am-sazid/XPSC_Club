8bf8ad3ed499002e7f368cdd23fe7873de31b4aa
am-sazid <amsazid.cse@gmail.com>
2026-05-01 22:01:09 +0600

    Map

---
+++ Week - 2/map.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    map<int, int> mp;
+
+    // mp.insert({"Sazid", 237759});
+    // mp["sazid"] = 237759;
+
+    // cout<<mp["sazid"]<<endl;
+
+    mp[10] = 20;
+    mp[8] = 30;
+    mp[6] = 25;
+
+    // for(auto [key,value] : mp){
+    //     cout<< key << " -> " << value<<endl;
+    // }
+
+    // for (auto it : mp)
+    // {
+    //     int key = it.first, value = it.second;
+
+    //     cout<< key << " -> " << value<<endl;
+    // }
+
+    int x; cin>> x ;
+    auto it = mp.find(x);
+    if(it != mp.end())
+        cout<< it->first << "  " << it->second << endl;
+    else{
+        cout<<"Key Not found";
+    }
+    return 0;
+}
