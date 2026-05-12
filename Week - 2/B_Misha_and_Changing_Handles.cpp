ca5a364bfcc19cf1c8bcfe773be80efa9c242d89
am-sazid <amsazid.cse@gmail.com>
2026-05-12 23:37:43 +0600

    Week -2 D

--- Week - 2/B_Misha_and_Changing_Handles.cpp
+++ Week - 2/B_Misha_and_Changing_Handles.cpp
@@ -1,10 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() 
+int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int t;
+    cin >> t;
+
+    map<string, string> ans, has;
+
+    for (int i = 1; i <= t; i++)
+    {
+        string a, b;
+        cin >> a >> b;
+
+        if (has.find(a) != has.end())
+        {
+            string s = has[a];
+            ans[s] = b;
+            has.erase(a);
+            has[b] = s;
+        }
+        else
+        {
+            ans[a] = b;
+            has[b] = a;
+        }
+    }
+
+    cout << ans.size() << endl;
+    for (auto [x, y] : ans)
+    {
+        cout << x << " " << y << endl;
+    }
+
     return 0;
 }
