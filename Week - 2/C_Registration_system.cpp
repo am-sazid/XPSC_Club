8534183a1d91a8a4ba67b54ce86455c2d5d60ae5
am-sazid <amsazid.cse@gmail.com>
2026-05-14 14:39:22 +0600

    Solved

--- Week - 2/C_Registration_system.cpp
+++ Week - 2/C_Registration_system.cpp
@@ -9,29 +9,49 @@ int main()
     int n;
     cin >> n;
 
-    string user[n];
+    map<string, int> mp;
 
-    for (int i = 0; i < n; i++)
+    while (n--)
     {
-        cin>> user[i];
-    }
-    cout << "OK" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
+        string s;
+        cin >> s;
+
+        if (mp[s] == 0)
         {
-            if (user[i] == user[j])
-            {
-                cout << user[j] << 1 << endl;
-                break;
-            }
-            else
-            {
-                cout << "OK" << endl;
-                break;
-            }
+            cout << "OK" << endl;
+            mp[s] = 1;
+        }
+
+        else
+        {
+            cout << s << mp[s] << endl;
+            mp[s]++;
         }
     }
+
+    // string user[n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin>> user[i];
+    // }
+    // cout << "OK" << endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         if (user[i] == user[j])
+    //         {
+    //             cout << user[j] << 1 << endl;
+    //             break;
+    //         }
+    //         else
+    //         {
+    //             cout << "OK" << endl;
+    //             break;
+    //         }
+    //     }
+    // }
 
     return 0;
 }
