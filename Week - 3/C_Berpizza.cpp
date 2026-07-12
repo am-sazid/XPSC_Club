4b5a18d10b2cf5892354a0a3255f72e6bead7d1d
am-sazid <amsazid.cse@gmail.com>
2026-07-12 23:07:15 +0600

    Week 3 - E

---
+++ Week - 3/C_Berpizza.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, customerNo = 1;
+    cin >> n;
+    set<pair<int, int>> s;
+    multiset<pair<int, int>> ms;
+    vector<int> ans;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int type;
+        cin >> type;
+
+        if (type == 1)
+        {
+            int money;
+            cin >> money;
+            s.insert({customerNo, money});
+            ms.insert({money, -customerNo});
+            customerNo++;
+        }
+        else if (type == 2)
+        {
+            int pos = s.begin()->first, money = s.begin()->second;
+            ans.push_back(pos);
+            s.erase(s.begin());
+            ms.erase({money, -pos});
+        }
+        else
+        {
+            int pos = -ms.rbegin()->second, money = ms.rbegin()->first;
+            ans.push_back(pos);
+            ms.erase(--ms.end());
+            s.erase({pos, money});
+        }
+    }
+
+    for (auto x : ans)
+        cout << x << " ";
+
+    cout << "\n";
+
+    return 0;
+}
